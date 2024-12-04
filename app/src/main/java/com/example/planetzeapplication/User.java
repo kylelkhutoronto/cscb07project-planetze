cae9cbc9959d2294475ff2705f4ee9a6527b6ca6
Anikait <a.kadamb@mail.utoronto.ca>
2024-12-03 23:42:02 -0500

    Update User.java

--- app/src/main/java/com/example/planetzeapplication/User.java
+++ app/src/main/java/com/example/planetzeapplication/User.java
@@ -1,29 +1,2 @@
-package com.example.planetzeapplication;
 
-public class User {
-    private String fullName;
-    private String email;
-    private Boolean doneSurvey;
-
-    public User() {
-    }
-
-    public User(String fullName, String email) {
-        this.fullName = fullName;
-        this.email = email;
-        this.doneSurvey = false;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public Boolean getDoneSurvey() {
-        return doneSurvey;
-    }
-
-}
 
